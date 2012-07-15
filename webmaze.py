e58c5b108999e908054af6e00f1159315cca0bbe
snailtsunami <snail@solitarysnail.com>
2012-07-14 17:51:16 -0700

    Faster generation; random number of doors per room.

--- webmaze.py
+++ webmaze.py
@@ -1,8 +1,22 @@
 import random
 
+def generateDoors(doorList,startNumber,endNumber,minNumberofDoors,maxNumberofDoors):
+	numberofDoors = random.randrange(minNumberofDoors,maxNumberofDoors)
+	i = 1
+	while i <= numberofDoors:
+		roomNumber = random.randrange(startNumber,endNumber)
+		doorList.append(roomNumber)
+		i = i + 1
+	return doorList
 
+#maze overall
 startNumber = 1
 endNumber = 10000
+
+#per room
+minNumberofDoors = 2
+maxNumberofDoors = 13
+
 
 roomNumberList = []
 
@@ -21,41 +35,37 @@ print "exitRoom" + str(exitRoom)
 
 roomNumberPath = roomNumberList[:]
 
+watchNumber = 1 #debugging
+
 for each in roomNumberList:
     
-    doorList = []
+	doorList = []
+	doorList = generateDoors(doorList,startNumber,endNumber,minNumberofDoors,maxNumberofDoors)
 
-    try:
-        pathDoor = roomNumberPath.pop(1)
-        doorList.append(pathDoor)
-    except IndexError:
-        pass
-    randomDoor1 = random.randrange(startNumber,endNumber)
-    doorList.append(randomDoor1)
-    randomDoor2 = random.randrange(startNumber,endNumber)
-    doorList.append(randomDoor2)
-
+	try:
+		pathDoor = roomNumberPath.pop(1)
+		doorList.append(pathDoor)
+	except IndexError:
+		pass
     
-    room = "rooms/room" + str(each) + ".html"
-    file = open(room,"w+")
-    if each == enterRoom:
-        file.write("WELCOME TO THE MAZE<br />")
-        print "Enter " + str(each) + "."
-    if each == exitRoom:
-        file.write("YOU WIN!<br />")
-        print "Exit " + str(each) + "."
-        randomDoor3 = random.randrange(startNumber,endNumber)
-        doorList.append(randomDoor3)
+	room = "rooms/room" + str(each) + ".html"
+	file = open(room,"w+")
+	if each == enterRoom:
+		file.write("WELCOME TO THE MAZE<br />")
+		print "Enter " + str(each) + "."
+	if each == exitRoom:
+		file.write("YOU WIN!<br />")
+		print "Exit " + str(each) + "."
         
-    a = 1
-    while a < endNumber:
-        b = random.randrange(0,1)
-        if b == 1:
-            random.shuffle(doorList)
-        a = a + 1
+	random.shuffle(doorList)
         
-    for door in doorList:
-        aHrefElement = '<a href="room' + str(door) + '.html">A door leads out of this room.</a>'
-        file.write(aHrefElement)
-        file.write("</br>")
-    file.close()
+	for door in doorList:
+		aHrefElement = '<a href="room' + str(door) + '.html">A door leads out of this room.</a>'
+		file.write(aHrefElement)
+		file.write("</br>")
+	file.close()
+
+	watchTest = watchNumber/1000.0 #debugging
+	if watchTest%1 == 0: #debugging
+		print watchNumber #debugging
+	watchNumber = watchNumber + 1 #debugging
